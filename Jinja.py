bdd9b70bac80d6443433c09bf331c52154cb0c5b
Goudot <florian.goudot@gmail.com>
2021-08-30 01:07:29 +0200

    improve telegraf ui & map button

--- Jinja.py
+++ Jinja.py
@@ -1,15 +1,21 @@
 # This Python file uses the following encoding: utf-8
 from jinja2 import Environment, FileSystemLoader
 from PySide6.QtWidgets import QApplication, QMainWindow, QCheckBox, QFileDialog
+from PySide6.QtCore import Qt
 
 
 class JinjaMaker:
     es_username = None
     es_password = None
-    es_host_list = None
+    es_host_list = [
+        "https://grafana.ether-source.fr:9201",
+        "https://grafana.ether-source.fr:9202",
+        "https://grafana.ether-source.fr:9203"
+        ]
     name = None
     telegraf_log_file_path = None
     os_is_windows = False
+    custom_process_enabled = False
     process_list_to_harvest = ["telegraf"]
     nvidia_smi_enabled = False
     nvidia_smi_bin_path = None
@@ -19,27 +25,79 @@ class JinjaMaker:
     chia_private_full_node_key_path = None
     t_rex_enabled = False
     api_lfdm = None
-
     def __init__(self):
         pass
+
 
     def SetEsUsername(self, username):
         self.es_username = username
 
-    def SetSmiBinPath(self, path):
+    def SetEsPassword(self, password):
+        self.es_username = password
+
+    def SetSmiBinPath(self):
         self.nvidia_smi_bin_path = QFileDialog.getOpenFileName()
 
-    def SetChiaCrtPath(self, path):
+    def SetChiaCrtPath(self):
         self.nvidia_smi_bin_path = QFileDialog.getOpenFileName()
 
-    def SetChiaKeyPath(self, path):
+    def SetChiaKeyPath(self):
         self.nvidia_smi_bin_path = QFileDialog.getOpenFileName()
 
-    def generate_file():
-        env = Environment(loader=FileSystemLoader('templates'))
-        template = env.get_template('test.html')
-        output_from_parsed_template = template.render(foo='Hello World!')
-        print(output_from_parsed_template)
-        # to save the results
-        with open("my_new_file.html", "w") as fh:
+    def SetCustomName(self, customName):
+        self.name = customName
+
+    def SetLaucherId(self, launcher_id):
+        self.launcher_id = launcher_id
+
+    def SetOsIsWindows(self, state):
+        if (Qt.Checked == state):
+            self.os_is_windows = True
+        else:
+            self.os_is_windows = False
+
+    def SetSmiEnable(self, state):
+        if (Qt.Checked == state):
+            self.nvidia_smi_enabled = True
+        else:
+            self.nvidia_smi_enabled = False
+
+    def SetCollectChiaEnable(self, state):
+        if (Qt.Checked == state):
+            self.chia_collect_info_enabled = True
+        else:
+            self.chia_collect_info_enabled = False
+
+    def GetCollectChiaEnable(self):
+        return self.chia_collect_info_enabled
+
+    def SetTrexEnable(self, state):
+        if (Qt.Checked == state):
+            self.t_rex_enabled = True
+        else:
+            self.t_rex_enabled = False
+
+    def SetCustomProcessEnable(self, state):
+        if (Qt.Checked == state):
+            self.custom_process_enabled = True
+        else:
+            self.custom_process_enabled = False
+
+    def GetCustomProcessEnable(self):
+        return self.custom_process_enabled
+
+    def SetTelegrafLogFilePath(self, telegraf_log_file_path):
+        self.telegraf_log_file_path = telegraf_log_file_path
+
+    def AppendToProcessList(self, process_list_to_harvest):
+        tmpList = process_list_to_harvest.split(' ')
+        for item in tmpList:
+            self.process_list_to_harvest.append(item)
+
+    def GenerateTemplate(self):
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template('telegraf.conf.jinja')
+        print(self.es_username)
+        output_from_parsed_template = template.render(es_username=self.es_username)
+        with open("telegraf.conf", "w") as fh:
             fh.write(output_from_parsed_template)
--- widget.py
+++ widget.py
@@ -7,7 +7,7 @@ from Jinja import JinjaMaker
 
 
 ########################################################################
-class RuntimeStylesheets(QMainWindow, QtStyleTools, JinjaMaker):
+class LauncherUi(QMainWindow, QtStyleTools, JinjaMaker):
     # ----------------------------------------------------------------------
     def __init__(self):
         """"""
@@ -21,6 +21,36 @@ class RuntimeStylesheets(QMainWindow, QtStyleTools, JinjaMaker):
         self.main.LoadCrt.clicked.connect(self.SetChiaCrtPath)
         self.main.LoadKey.clicked.connect(self.SetChiaKeyPath)
         self.main.SmiBinPath.clicked.connect(self.SetSmiBinPath)
+
+        self.main.ChiaEnabled.stateChanged.connect(self.DisplayChiaWidget)
+        self.main.SmiEnabled.stateChanged.connect(self.DisplaySmiWidget)
+        self.main.TRexEnabled.stateChanged.connect(self.SetTrexEnable)
+        self.main.CustomProcessEnabled.stateChanged.connect(self.DisplayCustomProcessWidget)
+
+        self.main.GenerateJinja.clicked.connect(self.FillJinjaTemplate)
+
+    def DisplayChiaWidget(self, state):
+        self.main.ChiaWidget.setHidden(not state)
+        self.SetCollectChiaEnable(state)
+
+    def DisplaySmiWidget(self, state):
+        self.main.SMIWidget.setHidden(not state)
+        self.SetSmiEnable(state)
+
+    def DisplayCustomProcessWidget(self, state):
+        self.main.CustomProcessWidget.setHidden(not state)
+        self.SetCustomProcessEnable(state)
+
+    def FillJinjaTemplate(self):
+        self.SetEsUsername(self.main.UsernameEs.text())
+        self.SetEsPassword(self.main.Password.text())
+        self.SetCustomName(self.main.CustomName.text())
+        if self.GetCollectChiaEnable():
+            self.SetLaucherId(self.main.LauncherId.text())
+        if self.GetCustomProcessEnable():
+            self.AppendToProcessList(self.main.ProcessInput.text())
+        print(self.es_username)
+        self.GenerateTemplate()
 
     def button_clicked(self):
         dialog = QFileDialog()
@@ -42,6 +72,9 @@ class RuntimeStylesheets(QMainWindow, QtStyleTools, JinjaMaker):
 if __name__ == "__main__":
     app = QApplication()
     app.setQuitOnLastWindowClosed(False)
-    frame = RuntimeStylesheets()
+    frame = LauncherUi()
+    frame.main.ChiaWidget.setHidden(True)
+    frame.main.SMIWidget.setHidden(True)
+    frame.main.CustomProcessWidget.setHidden(True)
     frame.main.show()
     app.exec()
